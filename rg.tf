7ce0dc47472ccc9069427f462cbcf21351c64a1e
vikranthsuravarapu <47105466+vikranthsuravarapu@users.noreply.github.com>
2022-12-29 18:13:11 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,9 +1,20 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
 provider "azurerm" {
-  version = "~>2.0"
   features {}
 }
-resource "azurerm_resource_group" "rename" {
-  name     = "var.rgname"
-  location = "var.location"
-  
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
 }
