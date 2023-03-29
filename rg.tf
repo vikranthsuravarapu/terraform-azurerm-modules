b5d7b8490d142204a775cc1a61e58a8bbf9fcd58
Vikranth <v-viksur@outlook.com>
2023-03-29 09:55:42 +0530

    commit

--- rg.tf
+++ rg.tf
@@ -1,20 +1,39 @@
-# Configure the Azure provider
 terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "Localtestvik"
+
+    workspaces {
+      name = "testlocal"
+    }
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
+     
     }
   }
-
-  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
-  features = {}
+ subscription_id = "57cc9967-b52d-414d-b422-b0791a491704"
+ client_id       = "bac413bf-a608-4495-a89a-6095443272d1"
+  client_secret   = "6aW8Q~JtTLwmlUI4ukiogyONzPSSf5~ytHVVzbit"
+  tenant_id       = "513294a0-3e20-41b2-a970-6d30bf1546fa"
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
+  name     = "testRG"
   location = "westus2"
 }
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/28"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
